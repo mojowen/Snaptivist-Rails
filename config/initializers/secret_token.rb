34e04b837c5cbe1f04c684b3a440d68b0b65c800
Scott Duncombe <srduncombe@gmail.com>
2013-06-26 16:09:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnaptivistRails::Application.config.secret_token = '656c09cb989798fb080ef1e418086ad0b18135bf9a589411d12315efd3dd0d31ae302df69d049e6e98c0712207747ea042dd56d52435cfa9c5e90bfa55348654'
