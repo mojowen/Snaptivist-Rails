68e3c8ec28e9ddbf28f94bb94b5444bef8f564d1
Scott Duncombe <srduncombe@gmail.com>
2013-07-03 09:07:57 -0700

    adding devise

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SnaptivistRails::Application.config.secret_token = '656c09cb989798fb080ef1e418086ad0b18135bf9a589411d12315efd3dd0d31ae302df69d049e6e98c0712207747ea042dd56d52435cfa9c5e90bfa55348654'
+SnaptivistRails::Application.config.secret_token = ENV['SECRET'] || '06a955419ede82a5253e73ddec34626f95460f84697588f68f46a9798d2c886dbee8355f7afafa134d734184a9903c88e14f66676b1ff130c4fab81262178b70'
