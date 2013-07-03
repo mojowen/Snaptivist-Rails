909e21b977d78b70fa4a4d141fdf6f50c864c7a3
Scott Duncombe <srduncombe@gmail.com>
2013-07-03 09:06:26 -0700

    can show photos stored in tmp directory

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
 	end
 	def save
 		signup = Signup.new( params[:signup] )
-		render :json => {:success =>  signup.save }
+		render :json => {:success =>  signup.save, :signup => params[:signup] }
+	end
+	def photo
+		render :text => File.open( 'tmp/'+params[:photo_name]+'.png','rb' ).read
 	end
 end
