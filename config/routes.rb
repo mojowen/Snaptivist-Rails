83b0e1fc8882e041f8762f4ae4062f50bdaa1f75
Scott Duncombe <srduncombe@gmail.com>
2013-07-03 09:09:00 -0700

    routes for devise / photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SnaptivistRails::Application.routes.draw do
+  devise_for :users, :controllers => {:omniauth_callbacks => "my_omniauth_callbacks"}
+
   match '/save' => 'home#save'
   match '/' => 'home#home'
+  match '/:photo_name' => 'home#photo', :via => :get
 end
