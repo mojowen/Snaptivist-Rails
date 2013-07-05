013e1f3d8f459059c2e9b224027336d90c36cdb7
Scott Duncombe <srduncombe@gmail.com>
2013-07-04 22:03:04 -0700

    if twitter throws error - try to post or just skip

--- app/models/status.rb
+++ app/models/status.rb
@@ -7,18 +7,25 @@ class Status < ActiveRecord::Base
 
 	def send_tweet
 
-	  	if photo_path.nil? && ! signup.photo_path.nil? #  Check and see if this status does not have a photo BUT this signup DOES have a photo
+		begin
+		  	if photo_path.nil? && ! signup.photo_path.nil? #  Check and see if this status does not have a photo BUT this signup DOES have a photo
 
-	  		photo = open( signup.photo_path) # If not - download it from facebook
+		  		photo = open( signup.photo_path) # If not - download it from facebook
 
-		  	self.data = Twitter.update_with_media( self.message, photo )
-		  	signup.statuses.reject{ |s| s == self }.each{ |s| s.update_attributes( :photo_path => self.data.media.first[:url] ) } # Attach the photo URL - already uplaoded to twitter
+		  		begin
+			  		self.data = Twitter.update_with_media( self.message, photo )
+				  	signup.statuses.reject{ |s| s == self }.each{ |s| s.update_attributes( :photo_path => self.data.media.first[:url] ) } # Attach the photo URL - already uplaoded to twitter
+			  	rescue
+			  		self.data = Twitter.update( self.message )
+			  	end
 
-		else # Photo path is set or there's no photo - either way not doing a media post
+			else # Photo path is set or there's no photo - either way not doing a media post
 
-			self.message += ' '+self.photo_path unless self.photo_path.nil? # Attach photo URL if set
-			self.data = Twitter.update(message)
+				self.message += ' '+self.photo_path unless self.photo_path.nil? # Attach photo URL if set
+				self.data = Twitter.update(message)
 
+			end
+		rescue
 		end
 
 		self.sent = true
