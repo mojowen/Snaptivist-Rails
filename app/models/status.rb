3af3871bc372f4501ef9a952dd91eaa1dab38fea
Scott Duncombe <srduncombe@gmail.com>
2013-07-11 14:32:03 -0700

    fixing twitter send maybe?

--- app/models/status.rb
+++ app/models/status.rb
@@ -7,19 +7,18 @@ class Status < ActiveRecord::Base
 
 	def send_tweet
 
-		# begin
-		  	if photo_path.nil? && ! signup.photo_path.nil? #  Check and see if this status does not have a photo BUT this signup DOES have a photo
+		begins
+			if photo_path.nil? && ! signup.photo_path.nil? #  Check and see if this status does not have a photo BUT this signup DOES have a photo
 				require 'RMagick'
-		  		photo = open( signup.photo_path) # If not - download it from facebook
 				image = Magick::ImageList.new
-				image.from_blob(photo.read)
-				image.resize_to_fit(600,450)
-		  		# begin
-			  		self.data = Twitter.update_with_media( self.message, image )
-				  	signup.statuses.reject{ |s| s == self }.each{ |s| s.update_attributes( :photo_path => self.data.media.first[:url] ) } # Attach the photo URL - already uplaoded to twitter
-			  	# rescue
-			  	# 	self.data = Twitter.update( self.message )
-			  	# end
+				image.from_blob( open( signup.photo_path).read )
+				image.resize(600,450)
+				begin
+					self.data = Twitter.update_with_media( self.message, image.to_blob )
+					signup.statuses.reject{ |s| s == self }.each{ |s| s.update_attributes( :photo_path => self.data['entities']['media'].first['url'] ) } # Attach the photo URL - already uplaoded to twitter
+				rescue
+					self.data = Twitter.update( self.message )
+				end
 
 			else # Photo path is set or there's no photo - either way not doing a media post
 
@@ -27,8 +26,8 @@ class Status < ActiveRecord::Base
 				self.data = Twitter.update(message)
 
 			end
-		# rescue
-		# end
+		rescue
+		end
 
 		self.sent = true
 		self.save
