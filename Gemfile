157277759ac409b46db18ac557916e71a20c7cd8
Scott Duncombe <srduncombe@gmail.com>
2013-07-11 12:48:38 -0700

    try tweeting with image magic

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'twitter'
 gem 'omniauth-twitter'
 gem 'omniauth-facebook'
 gem 'aws-s3'
+gem 'rmagick'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/status.rb
+++ app/models/status.rb
@@ -11,9 +11,11 @@ class Status < ActiveRecord::Base
 		  	if photo_path.nil? && ! signup.photo_path.nil? #  Check and see if this status does not have a photo BUT this signup DOES have a photo
 
 		  		photo = open( signup.photo_path) # If not - download it from facebook
-
+				image = Magick::ImageList.new
+				image.from_blob(photo.read)
+				image.resize_to_fit(600,450)
 		  		# begin
-			  		self.data = Twitter.update_with_media( self.message, photo )
+			  		self.data = Twitter.update_with_media( self.message, image )
 				  	signup.statuses.reject{ |s| s == self }.each{ |s| s.update_attributes( :photo_path => self.data.media.first[:url] ) } # Attach the photo URL - already uplaoded to twitter
 			  	# rescue
 			  	# 	self.data = Twitter.update( self.message )
