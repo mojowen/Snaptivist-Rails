27ce279d37817b1b6d34c6b3fb9e3d4494953600
Scott Duncombe <srduncombe@gmail.com>
2013-07-16 16:01:32 -0700

    top for popup

--- app/models/status.rb
+++ app/models/status.rb
@@ -9,16 +9,15 @@ class Status < ActiveRecord::Base
 
 		begin
 			if photo_path.nil? && ! signup.photo_path.nil? #  Check and see if this status does not have a photo BUT this signup DOES have a photo
+				return 'over photo limit - skipping' unless Rails.cache.read('no_upload').nil?
+
 				require 'RMagick'
 				image = Magick::ImageList.new
 				image.from_blob( open( signup.photo_path.split('?')[0] ).read )
 				image = image.resize(600,450)
-				# begin
-					self.data = Twitter.update_with_media( self.message, image.to_blob )
-					signup.statuses.reject{ |s| s == self }.each{ |s| s.update_attributes( :photo_path => self.data['entities']['media'].first['url'] ) } # Attach the photo URL - already uplaoded to twitter
-				# rescue
-				# 	self.data = Twitter.update( self.message )
-				# end
+
+				self.data = Twitter.update_with_media( self.message, image.to_blob )
+				signup.statuses.reject{ |s| s == self }.each{ |s| s.update_attributes( :photo_path => self.data['entities']['media'].first['url'] ) } # Attach the photo URL - already uplaoded to twitter
 
 			else # Photo path is set or there's no photo - either way not doing a media post
 
@@ -32,9 +31,14 @@ class Status < ActiveRecord::Base
 			return 'Tweet Success'
 		rescue => e
 			return "Tweet Fail\nError: #{e}\nData:#{self.message} #{self.signup.photo_path}"
-			self.sent = true
-			self.data = { :error => e }
-			self.save
+
+			if "#{e}".index('daily photo limit')
+				Rails.cache.write('no_upload',true,:expires_in => 3.hours)
+			else
+				self.sent = true
+				self.data = { :error => e }
+				self.save
+			end
 		end
 
 	end
