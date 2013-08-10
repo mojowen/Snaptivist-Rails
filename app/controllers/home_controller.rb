e5e9d033ced21daea29a7114da48b029379d4a38
Scott Duncombe <srduncombe@gmail.com>
2013-08-09 20:19:11 -0700

    return error in response

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
 		if  signup.match_or_save
 			render :json => {:success => true, :signup => params[:signup] }, :template => false
 		else
-			render :json => {:success => false, :signup => params[:signup] }, :template => false, :status => 500
+			render :json => {:success => false, :signup => params[:signup], :errors => signup.errors }, :template => false, :status => 500
 		end
 	end
 
